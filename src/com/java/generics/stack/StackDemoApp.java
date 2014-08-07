d732f06194189245357e580a5fce1450bd57b5d7
WhiteDev <bloggerprod@gmail.com>
2014-08-07 15:03:03 -0400

    Stack

---
+++ src/com/java/generics/stack/StackDemoApp.java
@@ -0,0 +1,27 @@
+package com.java.generics.stack;
+
+public class StackDemoApp {
+
+    public static void main(String[] args) {
+
+    }
+    
+    class Stack<T> {
+	
+	// int[] Stack = new Stack[5];
+	T[] stack = (T[]) new Object[5];
+	private int ptr = -1;
+	
+	void push(T data) {
+	    ptr++;
+	    stack[ptr] = data;
+	}
+	
+	T pop() {
+	    return (T) stack[ptr--];
+	    
+	}
+	
+    }
+
+}

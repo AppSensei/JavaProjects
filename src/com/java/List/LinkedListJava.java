8db7651ed9113084072e3ee1904572b3e2abd28a
WhiteDev <bloggerprod@gmail.com>
2014-08-08 13:40:34 -0400

    Added a ListIterator Method.

--- src/com/java/List/LinkedListJava.java
+++ src/com/java/List/LinkedListJava.java
@@ -1,6 +1,7 @@
 package com.java.List;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 public class LinkedListJava {
@@ -10,18 +11,29 @@ public class LinkedListJava {
 	LinkedList<String> newStudents = new LinkedList<String>();
 
 	for (int i = 0; i <= 10; i++) {
-	    
+
 	    System.out.println("What is your name?");
 	    Scanner input = new Scanner(System.in);
 	    String newName = input.nextLine();
-	   
+
 	    newStudents.add(newName);
 	    System.out.println(newStudents);
 
 	}
 
+	// Using ListIterator().
+	// It is different from iterator because this can add elements.
 
+	ListIterator<String> it = newStudents.listIterator();
+	it.add("Mariam John");
+	it.add("Elza John");
+	it.add("Jasmine John");
+	it.add("Priya John");
 
+	while (it.hasNext()) {
+	    String string = (String) it.next();
+	    System.out.println(string);
+	}
 
     }
 }

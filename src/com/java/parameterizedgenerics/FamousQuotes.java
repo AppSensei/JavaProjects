9871299a316d9479cd9cbaf709367fdf503e4a5c
WhiteDev <bloggerprod@gmail.com>
2014-08-11 11:36:42 -0400

    Removed invalid collections import.

--- src/com/java/parameterizedgenerics/FamousQuotes.java
+++ src/com/java/parameterizedgenerics/FamousQuotes.java
@@ -2,7 +2,6 @@ package com.java.parameterizedgenerics;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 
 public class FamousQuotes {
